f94d2f5b5f3a070d801f85a78b2c3118798ce121
hema3104 <reddyhema663@gmail.com>
2026-03-03 14:16:17 +0530

    paranthesis added in line "19"

--- app/src/main/java/com/example/contactapp/presentation/ContactViewModel.kt
+++ app/src/main/java/com/example/contactapp/presentation/ContactViewModel.kt
@@ -16,7 +16,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ContactViewModel @Inject constructor(()
+class ContactViewModel @Inject constructor(
     private val dataBase: ContactDataBase
 ) : ViewModel() {
 
